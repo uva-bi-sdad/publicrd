cf314ce952287eff7fd3e8ec212e62f0dde0bf76
Sean Pietrowicz <spietrow@nd.edu>
2019-11-22 14:55:11 +0000

    Spacing change

--- src/fall_federal-reporter/abstracts_federal-reporter.R
+++ src/fall_federal-reporter/abstracts_federal-reporter.R
@@ -8,7 +8,7 @@ library(ggplot2)
 ##Federal Reporter Abstract Profiling
 #Checking Federal Reporter FY2008-FY2018
 
-#Import Abstract Files
+#Import Abstract Files:
 
 X2008_abstracts_federal_reporter <- read_csv("data/original/federal-reporter_abstracts/2008-abstracts_federal-reporter.csv")
 X2009_abstracts_federal_reporter <- read_csv("data/original/federal-reporter_abstracts/2009-abstracts_federal-reporter.csv")
@@ -21,6 +21,21 @@ X2015_abstracts_federal_reporter <- read_csv("data/original/federal-reporter_abs
 X2016_abstracts_federal_reporter <- read_csv("data/original/federal-reporter_abstracts/2016-abstracts_federal-reporter.csv")
 X2017_abstracts_federal_reporter <- read_csv("data/original/federal-reporter_abstracts/2017-abstracts_federal-reporter.csv")
 X2018_abstracts_federal_reporter <- read_csv("data/original/federal-reporter_abstracts/2018-abstracts_federal-reporter.csv")
+
+#Tag with year:
+
+X2008_abstracts_federal_reporter$FY <-2008
+X2009_abstracts_federal_reporter$FY <-2009
+X2010_abstracts_federal_reporter$FY <-2010
+X2011_abstracts_federal_reporter$FY <-2011
+X2012_abstracts_federal_reporter$FY <-2012
+X2013_abstracts_federal_reporter$FY <-2013
+X2014_abstracts_federal_reporter$FY <-2014
+X2015_abstracts_federal_reporter$FY <-2015
+X2016_abstracts_federal_reporter$FY <-2016
+X2017_abstracts_federal_reporter$FY <-2017
+X2018_abstracts_federal_reporter$FY <-2018
+
 
 #Review whether there are any duplicate abstracts
 
@@ -38,17 +53,26 @@ abstracts_federal_reporter <- rbind(
   X2018_abstracts_federal_reporter
 )
 
+#Flag and tally duplicates:
+
 abstracts_federal_reporter$IS_DUPLICATED <- duplicated(abstracts_federal_reporter$ABSTRACT)
 unique_abstracts <- filter(abstracts_federal_reporter, IS_DUPLICATED == FALSE)
-
 percent_unique <- 100*nrow(unique_abstracts)/nrow(abstracts_federal_reporter)
 
-#There are no duplicate project codes, but there are duplicate abstracts 
 #53% of abstracts from 2008-2018 are unique, 47% are duplicates
+
+unique_abstracts <- unique_abstracts %>% 
+  select(-IS_DUPLICATED)
+
+#write.csv(abstracts_federal_reporter, file = "data/working/federal_reporter/duplicate_abstracts.csv", row.names = FALSE)
+#write.csv(unique_abstracts, file = "data/working/federal_reporter/abstracts_federal-reporter.csv", row.names = FALSE)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ##Examine Abstract Lengths:
 
 abstracts_federal_reporter$CHAR_LENGTH <- str_length(abstracts_federal_reporter$ABSTRACT)
+freq_charlength <- tally(abstracts_federal_reporter$CHAR_LENGTH)
 
 #Histogram 1:
 #Not clean looking. Only 300 out of a million abstracts are above 10,000
@@ -63,5 +87,3 @@ hist(abstracts_federal_reporter$CHAR_LENGTH,
      main = "Abstract Lengths",
      col = "blue"
      )
-
-#Clearly the vast majority of these abstracts are cut off at some point.
