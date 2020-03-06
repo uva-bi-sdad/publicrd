9ccc327fb2cc6373e4df0c10a56ee17de01ee7bc
Kathryn Linehan <kjl5t@virginia.edu>
2020-03-06 14:56:36 -0500

    Code as of 3/6

---
+++ src/Topic Modelling FR/LDAvariables.py
@@ -0,0 +1,22 @@
+import gensim
+
+def createLDAvars(docs):
+
+    # Create the variables needed for LDA from the lemmatized text: dictionary (id2word), corpus
+    
+    # Create Dictionary
+    id2word = gensim.corpora.Dictionary(docs)
+
+    keep_only_most_common=int(len(docs)/2) #LDA works best with less features than documents
+    #Filter words to only those found in at least a set number of documents (min_appearances)
+    id2word.filter_extremes(no_below=3, no_above=0.4, keep_n=keep_only_most_common)
+
+
+    # Creat Corpus (Term Document Frequency)
+
+    #Creates a count for each unique word appearing in the document, where the word_id is substituted for the word
+    corpus = [id2word.doc2bow(doc) for doc in docs]
+
+    return id2word, corpus
+
+
